36edc6e7175c49a5c41ea10e0fb35e34cba62b22
DESKTOP-CPIIR7V\likeit_student <mxm00011257@gmail.com>
2018-04-13 17:11:41 +0900

    Q4

---
+++ Q1.sql
@@ -0,0 +1,4 @@
+create table item_category (
+	category_id int primary key auto_increment,
+	category_name varchar(256) not null
+);
---
+++ Q3.sql
@@ -0,0 +1,3 @@
+insert into item_category(category_name) values('‰Æ‹ï');
+insert into item_category(category_name) values('H•i');
+insert into item_category(category_name) values('–{');
